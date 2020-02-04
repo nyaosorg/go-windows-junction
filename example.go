38e87e89f1ebb1698078b1d04880dbadc060b117
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-02-04 10:49:06 +0900

    example.go: call os.Readlink

--- example.go
+++ example.go
@@ -4,16 +4,28 @@ package main
 
 import (
 	"os"
+	"errors"
 
 	"github.com/zetamatta/go-windows-junction"
 )
 
-func main() {
+func main1() error {
 	if len(os.Args) < 3 {
-		println("go run example.go TARGET MOUNTPT")
-		return
+		return errors.New("go run example.go TARGET MOUNTPT")
 	}
 	if err := junction.Create(os.Args[1], os.Args[2]); err != nil {
+		return err
+	}
+	result, err := os.Readlink(os.Args[2])
+	if err != nil {
+		return err
+	}
+	println(os.Args[2], " is linked to ", result)
+	return nil
+}
+
+func main() {
+	if err := main1(); err != nil {
 		println(err.Error())
 		os.Exit(1)
 	}
